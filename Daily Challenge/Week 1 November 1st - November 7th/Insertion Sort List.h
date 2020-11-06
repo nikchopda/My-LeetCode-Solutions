9a0ce3f7e1c81e75f9b9186159151dd876c0861f
nikchopda <nikunjchopda1996@gmail.com>
2020-11-06 20:02:32 +0530

    Insertion Sort List

---
+++ Daily Challenge/Week 1 November 1st - November 7th/Insertion Sort List.h
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        if (!head){return NULL;}
+        ListNode *p=new ListNode(0);
+        p->next = head;
+        head = p;
+        ListNode *q;
+        
+        while (p->next!=NULL){
+            q = head;
+            bool flag = false;
+            while (q!=p){
+                if (q->next->val>p->next->val){
+                    ListNode* tmp1=p->next;
+                    p->next =p->next->next;
+                    tmp1->next = q->next;
+                    q->next = tmp1;
+                    flag =true;
+                    break;
+                }else{
+                    q=q->next;    
+                }
+            }
+            if (!flag){
+                p=p->next;
+            }
+        }
+        return head->next;
+    }
+};
